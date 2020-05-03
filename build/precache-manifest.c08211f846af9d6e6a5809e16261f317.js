bd2e207e0a14879514379f9665d62810db6f8d6a
Silvio Ronaldo <silvio.ronaldo77@gmail.com>
2020-05-03 20:25:21 -0300

    Configurando as rotas do fluxo

--- build/precache-manifest.c08211f846af9d6e6a5809e16261f317.js
+++ build/precache-manifest.c08211f846af9d6e6a5809e16261f317.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d50bc431e2a79324deff33808b81175d",
+    "revision": "97b5e641b213464b181a2aadc69351ba",
     "url": "/adam-interface/index.html"
   },
   {
-    "revision": "6c0707bd2336af3bde69",
+    "revision": "4be17df30906cd4c243e",
     "url": "/adam-interface/static/css/main.6b7f0bda.chunk.css"
   },
   {
-    "revision": "519feb113464b272d05c",
-    "url": "/adam-interface/static/js/2.14480b90.chunk.js"
+    "revision": "0941436eaba00aede9f6",
+    "url": "/adam-interface/static/js/2.90ccd82a.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/adam-interface/static/js/2.14480b90.chunk.js.LICENSE.txt"
+    "url": "/adam-interface/static/js/2.90ccd82a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6c0707bd2336af3bde69",
-    "url": "/adam-interface/static/js/main.e4df4001.chunk.js"
+    "revision": "4be17df30906cd4c243e",
+    "url": "/adam-interface/static/js/main.64e78385.chunk.js"
   },
   {
     "revision": "a1d02b0f8faa90d68243",
--- src/components/PostQuestion.js
+++ src/components/PostQuestion.js
@@ -1,16 +1,43 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import api from '../services/api';
 
-class PostQuestion extends Component {
-  render() {
+function PostQuestion() {
+    const[valueQuestion, setValueQuestion] = useState('');
+
+    async function readQuestion(e) {
+      e.preventDefault();
+      const response = await api.post('/storeQuestion/user/:userId/product/1', {
+        question: valueQuestion
+      });
+      console.log(response.data._id);
+
+      waitResponse(response.data.productId, response.data._id);
+    }
+
+    async function waitResponse(productID, questionID) {
+      const response = await api.post('/sendMessage/user/1/product/1', {
+        question: valueQuestion
+      });
+      
+      searchQuestion(productID, questionID, response.data.answer);
+    }
+
+    async function searchQuestion(productID, questionID, answer) {
+      await api.post(`/answerQuestion/product/${productID}/question/${questionID}`, {
+        answer: answer
+      });
+    }
+
     return (  
       <div className="question">
         <center>
-            <textarea placeholder="Escreva uma pergunta.." maxLength="2000"></textarea>
-            <input type="button" value="Perguntar"/>
+          <form onSubmit={(e) => readQuestion(e)}>
+            <textarea placeholder="Escreva uma pergunta.." maxLength="2000" onChange={e => setValueQuestion(e.target.value)}></textarea>
+            <input type="submit" value="Perguntar"/>
+          </form>
         </center>
       </div>
     );
-  }
 }
 
 export default PostQuestion;
