003e62f1fe5f92dbc99e184c0a654f856730813b
Silvio Ronaldo <silvio.ronaldo77@gmail.com>
2020-05-03 14:58:37 -0300

    Updates

---
+++ precache-manifest.7b4ac56c182532176399036ca67b911d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d50bc431e2a79324deff33808b81175d",
+    "url": "/adam-interface/index.html"
+  },
+  {
+    "revision": "6c0707bd2336af3bde69",
+    "url": "/adam-interface/static/css/main.6b7f0bda.chunk.css"
+  },
+  {
+    "revision": "519feb113464b272d05c",
+    "url": "/adam-interface/static/js/2.14480b90.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/adam-interface/static/js/2.14480b90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c0707bd2336af3bde69",
+    "url": "/adam-interface/static/js/main.e4df4001.chunk.js"
+  },
+  {
+    "revision": "a1d02b0f8faa90d68243",
+    "url": "/adam-interface/static/js/runtime-main.d675f89d.js"
+  }
+]);
