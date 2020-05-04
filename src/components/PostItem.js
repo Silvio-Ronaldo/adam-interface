bcd666f5c3c33802eb35e185dc78b2bb639a0b38
Joed Silva <joedsilva2016@gmail.com>
2020-05-04 13:32:14 -0300

    add modifications

--- src/components/PostItem.js
+++ src/components/PostItem.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import {
+  format,
+  parseISO
+} from 'date-fns';
 
 function PostItem({ question, createdAt, answer }) {
+
+  const [formattedDate, setformattedDate] = useState('');
+
+  useEffect(() => {
+    function formatDate() {
+      const parsedDate = parseISO(createdAt);
+      const date = format(
+        parsedDate,
+        "'Dia' dd/MM', às ' HH:mm'h'"
+      );
+
+      setformattedDate(date);
+    }
+
+    formatDate();
+  }, [createdAt])
+
   return (
     <div className="post">
       <div className="content">
         <p className="question">{question}</p>
-        <p><b>R:</b> {answer ? answer : 'Aguarde a resposta do vendedor.'} <span>{createdAt}</span></p>
+        <p><b>R:</b> {answer ? answer : 'Aguarde a resposta do vendedor.'} <span>{answer ? formattedDate : null}</span></p>
       </div>
       <hr />
     </div>
